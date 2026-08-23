8325045d11c57f34db07ba97ca753ca50b2b1ecd
TimothyGCY <timothy.gcy@outtlook.com>
2026-08-23 21:17:03 +0800

    add lazygit

--- lua/bleckshiba/plugins/git.lua
+++ lua/bleckshiba/plugins/git.lua
@@ -5,4 +5,18 @@ return {
   {
     'TimothyGCY/blamer.nvim',
   },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  }
 }
